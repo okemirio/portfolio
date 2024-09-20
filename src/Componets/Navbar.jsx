d7886ed4ab12ccb8bf6bc4a9003231904247baff
okemirio <okemirio@gmail.com>
2024-09-20 12:27:12 +0100

    production

--- src/Componets/Navbar.jsx
+++ src/Componets/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Navbar as BootstrapNavbar, Nav, Container } from "react-bootstrap";
-import Logo from "../../src/assets/images/logo.svg";
-import NavIcon1 from "../assets/images/nav-icon1.svg";
-import NavIcon2 from "../assets/images/nav-icon2.svg";
-import NavIcon3 from "../assets/images/nav-icon3.svg";
+import Logo from "/src/assets/images/logo.svg";
+import NavIcon1 from "/src/assets/images/nav-icon1.svg";
+import NavIcon2 from "/src/assets/images/nav-icon2.svg";
+import NavIcon3 from "/src/assets/images/nav-icon3.svg";
 
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState("home");
@@ -29,7 +29,7 @@ const Navbar = () => {
     <BootstrapNavbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <BootstrapNavbar.Brand href="#home">
-          {/* <img src={Logo} alt="Logo" /> */}
+          <img src={Logo} alt="Logo" />
         </BootstrapNavbar.Brand>
         <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
@@ -38,62 +38,45 @@ const Navbar = () => {
           <Nav className="me-auto">
             <Nav.Link
               href="#home"
-              className={
-                activeLink === "home" ? "active navbar-link" : "navbar-link"
-              }
+              className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
               onClick={() => onUpdateActiveLink("home")}
             >
               Home
             </Nav.Link>
             <Nav.Link
               href="#skills"
-              className={
-                activeLink === "skills" ? "active navbar-link" : "navbar-link"
-              }
+              className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
               onClick={() => onUpdateActiveLink("skills")}
             >
               Skills
             </Nav.Link>
             <Nav.Link
               href="#projects"
-              className={
-                activeLink === "projects" ? "active navbar-link" : "navbar-link"
-              }
+              className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
               onClick={() => onUpdateActiveLink("projects")}
             >
               Projects
             </Nav.Link>
             <Nav.Link
               href="#contact"
-              className={
-                activeLink === "contact" ? "active navbar-link" : "navbar-link"
-              }
+              className={activeLink === "contact" ? "active navbar-link" : "navbar-link"}
               onClick={() => onUpdateActiveLink("contact")}
             >
-              Contact
+              Contact Us
             </Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/onyedikachi-okemiri-29b199b3/">
-                <img src={NavIcon1} alt="Icon 1" />
+                <img src={NavIcon1} alt="LinkedIn Icon" />
               </a>
-              <a href="#">
-                <img src={NavIcon2} alt="Icon 2" />
+              <a href="https://github.com/your-profile">
+                <img src={NavIcon2} alt="GitHub Icon" />
               </a>
-              <Nav.Link
-                href="#contact"
-                className={
-                  activeLink === "contact"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("contact")}
-              >
-                Contact
-              </Nav.Link>
+              <a href="https://twitter.com/your-profile">
+                <img src={NavIcon3} alt="Twitter Icon" />
+              </a>
             </div>
-            {/* Button to download CV */}
             <a href="/CV.pdf" download className="vvd">
               <button>
                 <span>Download CV</span>
